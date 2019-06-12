da16c4f86da08d31225de05f4551e9fbb69ea301
koalabzium <zosia.owsiak@gmail.com>
2019-06-12 22:00:16 +0200

    Perfect standalone

---
+++ client/src/main/java/com/zosiaowsiak/parking/Views/AppManager.java
@@ -0,0 +1,15 @@
+package com.zosiaowsiak.parking.Views;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.util.List;
+
+@ManagedBean
+@SessionScoped
+public class AppManager {
+
+    public List<String> getMessages(){
+        return null;
+    }
+
+}
